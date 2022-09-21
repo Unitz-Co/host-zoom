df5c7befabf6e82424c64e6411b0066600fc8243
Hung Tran <hungtran0203@gmail.com>
2022-09-21 11:27:56 +0700

    update home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,13 +3,14 @@ import React from 'react';
 import { ctx } from '@vl/redata';
 import DIV from '@vl/redata/DIV.macro';
 
-import HomePage from '@uz/unitz-pages/CFHomeZoomPage';
+// import HomePage from '@uz/unitz-pages/CFHomeZoomPage';
 import withPageContext from '@uz/unitz-pages/withPageContext';
 import App from '@uz/unitz-app-web/ToolApp';
+import HomePage from '@uz/unitz-tool-pages/Home';
 
 import Layout from '@uz/unitz-layout-web/LayoutZoom';
 import SEO from '@uz/unitz-layout-web/SEO';
-import { Button } from '@uz/unitz-components-web/Button';
+import Announcement from '@uz/unitz-components-web/Announcement';
 
 import PageData from '../data/PageDataQuery';
 import '../styles/styles.css';
@@ -20,28 +21,13 @@ const HomeIndex = withPageContext((props) => {
       <Layout location={props.location} PageData={PageData}>
         <DIV>
           <SEO pageData={ctx.apply('ctf.findPage', { name: 'HomePageLanding' })} />
-          <Layout.POS name="app-header">{ctx.apply('ctf.renderSection', { name: 'articleNavbarSection' })}</Layout.POS>
+          {/* <Layout.POS name="app-header">{ctx.apply('ctf.renderSection', { name: 'articleNavbarSection' })}</Layout.POS> */}
+          <Layout.POS name="app-header">{ctx.apply('ctf.renderSection', { name: 'B2BNavbarSection' })}</Layout.POS>
           <Layout.POS name="app-body">
-            <div className="px-2">
-              <a
-                href={`https://zoom.us/oauth/authorize?response_type=code&client_id=pGEImldqTKi7EUaRC0FBhQ&redirect_uri=https%3A%2F%2Funitz-zoom.web.app%2Fzoom_oauth`}
-              >
-                Click to install
-              </a>
-              <Button
-                className="mx-2"
-                type="primary"
-                size="small"
-                onClick={() => {
-                  window.location.href = `https://us06web.zoom.us/j/7124174735?pwd=dWFvS1NxN1ZpWUxjbm41REk2c0NSZz09`;
-                }}
-              >
-                Join meeting
-              </Button>
-            </div>
+            <Announcement />
             <HomePage />
           </Layout.POS>
-          <Layout.POS name="app-footer">{ctx.apply('ctf.renderSection', { name: 'FooterSection' })}</Layout.POS>
+          {/* <Layout.POS name="app-footer">{ctx.apply('ctf.renderSection', { name: 'FooterSection' })}</Layout.POS> */}
         </DIV>
       </Layout>
     </App>
