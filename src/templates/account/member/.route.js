9d2469ef1d042706abea3d7d65124e0322e732a0
Hung Tran <hungtran0203@gmail.com>
2022-10-20 16:45:48 +0700

    update member route

--- src/templates/account/member/.route.js
+++ src/templates/account/member/.route.js
@@ -2,13 +2,13 @@ const _ = require('lodash');
 const { routeStore } = require('@vl/mod-utils/gatsbyRouteStore');
 const { getGbRoute } = require('@vl/hooks/useGbRoute');
 
-routeStore.addRule('toolAccountAdmin', {
+routeStore.addRule('toolAccountMember', {
   url: (params) => {
     params = _.merge({}, params, _.get(getGbRoute().getPageContext(), 'params'));
     const id = _.get(params, 'id');
     const slug = _.get(params, 'slug');
     if (slug) {
-      return `/${slug}/admin`;
+      return `/${slug}/members`;
     }
     return `/accounts/me/members`;
   },
@@ -20,6 +20,6 @@ routeStore.addRule('toolAccountAdmin', {
     return params;
   },
   match: (urlObject) => {
-    return urlObject.pathname === 'account/admin';
+    return urlObject.pathname === 'account/members';
   },
 });
