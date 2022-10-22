f36c21e57285e1c00621b2a5c1dcb09f2c71cf12
Hung Tran <hungtran0203@gmail.com>
2022-10-22 15:52:13 +0700

    account create

--- src/templates/account/create/.meta.js
+++ src/templates/account/create/.meta.js
@@ -6,25 +6,66 @@ const { withLocale } = require('@uz/mod-translations/utils-biz');
 require('@vl/mod-config/builder');
 require('@vl/mod-config/web').loadEnv();
 
+const hasuraClient = require('@vl/mod-clients/hasuraCtf');
+
+const getAllAccounts = async () => {
+  const query = hasuraClient.gql`
+    query account {
+      account: b2b_account {
+        id
+        slug
+        account_profile {
+          id
+          display_name
+          avatar_url
+        }
+      }
+    }
+  `;
+  try {
+    const rtn = await hasuraClient.getClient().request(query);
+
+    const data = _.get(rtn, 'account', []);
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+  return [];
+};
+
 exports.createPages = withLocale(async function(item, gatsby) {
   // return [];
   const localeConfig = this;
-  const accountSlug = routeStore.toUrl('toolCreateAccount', account);
-  const accountPath = localeConfig.langSlug(path.join('/', accountSlug));
-  console.log('creating page', accountPath);
-  const pageContext = _.cloneDeep({
-    id: accountId,
-    accountId,
-    slug: accountSlug,
-    lang: localeConfig.get('lang'),
-    params: {
-      ...account,
-    },
-  });
-  return gatsby.actions.createPage({
-    path: accountPath,
-    component: item.resolvers.component(gatsby),
-    context: pageContext,
-  });
+  const accounts = await getAllAccounts();
 
+  return Promise.all(
+    accounts.map((accountData) => {
+      const accountId = _.get(accountData, 'id');
+      const account = {
+        ...accountData,
+        accountId,
+        profile: {
+          ...accountData.account_profile,
+        },
+      };
+
+      const accountSlug = routeStore.toUrl('toolCreateAccount', account);
+      const accountPath = localeConfig.langSlug(path.join('/', accountSlug));
+      console.log('creating page', accountPath);
+      const pageContext = _.cloneDeep({
+        id: accountId,
+        accountId,
+        slug: accountSlug,
+        lang: localeConfig.get('lang'),
+        params: {
+          ...account,
+        },
+      });
+      return gatsby.actions.createPage({
+        path: accountPath,
+        component: item.resolvers.component(gatsby),
+        context: pageContext,
+      });
+    })
+  );
 });
