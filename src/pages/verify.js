44784b2e6eee2080a51fa2e06c246cb7f87605f4
anhphong-2010 <anhphongnw.2010@gmail.com>
2023-01-13 12:37:10 +0700

    ^ verify mail page

---
+++ src/pages/verify.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { ctx } from '@vl/redata';
+import DIV from '@vl/redata/DIV.macro';
+
+import VerifyEmailPage from '@uz/unitz-tool-pages/VerifyEmailPage';
+
+import App from '@uz/unitz-app-web/ToolApp';
+import SEO from '@uz/unitz-layout-web/SEO';
+
+import Layout from '@uz/unitz-layout-web/LayoutMain';
+import withPageContext from '@uz/unitz-tool-pages/withPageContext';
+import PageData from '../data/PageDataQuery';
+
+const VerifyIndex = withPageContext((props) => {
+  return (
+    <App>
+      <Layout location={props.location} PageData={PageData}>
+        <DIV>
+          <SEO pageData={ctx.apply('ctf.findPage', { name: 'AuthPage' })} />
+          <Layout.POS name="app-header">{ctx.apply('ctf.renderSection', { name: 'HomeBizNavbarSection' })}</Layout.POS>
+          <Layout.POS name="app-body">
+            <div className="wrapper app-row">
+              <VerifyEmailPage />
+            </div>
+          </Layout.POS>
+          <Layout.POS name="app-footer">{ctx.apply('ctf.renderSection', { name: 'AuthBizFooterSection' })}</Layout.POS>
+        </DIV>
+      </Layout>
+    </App>
+  );
+});
+
+export default VerifyIndex;
