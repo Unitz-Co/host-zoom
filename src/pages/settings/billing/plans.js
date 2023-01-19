1fce42449e2d6e32f38050a2f0cb34c3a9d14715
Hung Tran <hungtran0203@gmail.com>
2023-01-19 15:26:01 +0700

    billing plans page

--- src/pages/settings/billing/plans.js
+++ src/pages/settings/billing/plans.js
@@ -10,6 +10,7 @@ import _ from 'lodash';
 import { ctx } from '@vl/redata';
 import DIV from '@vl/redata/DIV.macro';
 import displayName from '@vl/redata/displayName.macro';
+import PlansPage from '@uz/unitz-tool-components/B2BBilling/Plans';
 
 // import PlansPage from '@uz/unitz-tool-pages/PlansPage';
 
@@ -38,99 +39,99 @@ const getClientToken = async () => {
   }
 };
 
-const PlansPage = () => {
-  const ref = React.useRef({});
-  const [bindStatus, $bindStatus] = React.useState(false);
-  const loadedStatus = useScript('https://js.braintreegateway.com/web/dropin/1.33.7/js/dropin.min.js');
+// const PlansPage = () => {
+//   const ref = React.useRef({});
+//   const [bindStatus, $bindStatus] = React.useState(false);
+//   const loadedStatus = useScript('https://js.braintreegateway.com/web/dropin/1.33.7/js/dropin.min.js');
 
-  React.useEffect(() => {
-    if (loadedStatus === 'ready') {
-      const braintree = window.braintree;
-      if (braintree) {
-        (async () => {
-          const clientToken = await getClientToken();
-          if (!clientToken) {
-            return;
-          }
-          console.log('clientTokenclientToken', clientToken);
-          braintree.dropin.create({
-            authorization: clientToken,
-            container: '#dropin-container',
-            threeDSecure: true,
-            // paypal: {
-            //   flow: 'vault',
-            // },
-            card: {
-              cardholderName: {
-                required: false
-              }
-            }
-            // locale: 'de_DE',
-          }, (createErr, instance) => {
-            $bindStatus(true);
-            ref.current.instance = instance;
-          });
-        })();
-      }
-    }
-  }, [loadedStatus]);
+//   React.useEffect(() => {
+//     if (loadedStatus === 'ready') {
+//       const braintree = window.braintree;
+//       if (braintree) {
+//         (async () => {
+//           const clientToken = await getClientToken();
+//           if (!clientToken) {
+//             return;
+//           }
+//           console.log('clientTokenclientToken', clientToken);
+//           braintree.dropin.create({
+//             authorization: clientToken,
+//             container: '#dropin-container',
+//             threeDSecure: true,
+//             // paypal: {
+//             //   flow: 'vault',
+//             // },
+//             card: {
+//               cardholderName: {
+//                 required: false
+//               }
+//             }
+//             // locale: 'de_DE',
+//           }, (createErr, instance) => {
+//             $bindStatus(true);
+//             ref.current.instance = instance;
+//           });
+//         })();
+//       }
+//     }
+//   }, [loadedStatus]);
 
-  const onRequestPaymentMethod = React.useCallback(async () => {
-    const { instance } = ref.current;
-    if (instance) {
-      const authModel = getREF().getRef('authModel');
-      const user_id = await authModel?.getUserId();
-      const accountModel = getREF().getRef('accountModel');
-      const account_id = await accountModel?.getAccountId();
-      await new Promise((resolve, reject) => {
-        instance.requestPaymentMethod({
-          threeDSecure: {
-            amount: '199.00',
-            email: 'hungtran0203@gmail.com',
-            billingAddress: {
-              givenName: 'Hung',
-              surname: 'Tran',
-              phoneNumber: '123-456-7890',
-              streetAddress: '123 XYZ Street',
-              extendedAddress: '',
-              locality: 'Anytown',
-              region: 'IL',
-              postalCode: '12345',
-              countryCodeAlpha2: 'US'
-            }
-          },
-        }, async (err, payload) => {
-          if (err) {
-            return reject(err);
-          }
-          console.log('payload', payload, user_id, account_id);
-          const resData = await fbFnsClient.getClient().post('payment-createSubscription', {
-            ...(user_id ? { user_id } : {}),
-            ...(account_id ? { account_id } : {}),
-            plan_id: 'unitz_plan_professional',
-            paymentMethodNonce: payload.nonce,
-          });
-          console.log('resData', resData);
-          resolve(payload);
-        });
-      });
-    }
-  }, [ref]);
+//   const onRequestPaymentMethod = React.useCallback(async () => {
+//     const { instance } = ref.current;
+//     if (instance) {
+//       const authModel = getREF().getRef('authModel');
+//       const user_id = await authModel?.getUserId();
+//       const accountModel = getREF().getRef('accountModel');
+//       const account_id = await accountModel?.getAccountId();
+//       await new Promise((resolve, reject) => {
+//         instance.requestPaymentMethod({
+//           threeDSecure: {
+//             amount: '199.00',
+//             email: 'hungtran0203@gmail.com',
+//             billingAddress: {
+//               givenName: 'Hung',
+//               surname: 'Tran',
+//               phoneNumber: '123-456-7890',
+//               streetAddress: '123 XYZ Street',
+//               extendedAddress: '',
+//               locality: 'Anytown',
+//               region: 'IL',
+//               postalCode: '12345',
+//               countryCodeAlpha2: 'US'
+//             }
+//           },
+//         }, async (err, payload) => {
+//           if (err) {
+//             return reject(err);
+//           }
+//           console.log('payload', payload, user_id, account_id);
+//           const resData = await fbFnsClient.getClient().post('payment-createSubscription', {
+//             ...(user_id ? { user_id } : {}),
+//             ...(account_id ? { account_id } : {}),
+//             plan_id: 'unitz_plan_professional',
+//             paymentMethodNonce: payload.nonce,
+//           });
+//           console.log('resData', resData);
+//           resolve(payload);
+//         });
+//       });
+//     }
+//   }, [ref]);
 
-  if (loadedStatus !== 'ready') {
-    return null;
-  }
-  return (
-    <div>
-      <div id="dropin-container"></div>
-      {!!bindStatus &&
-        <Button onClick={onRequestPaymentMethod}>
-          Request payment method
-        </Button>
-      }
-    </div>
-  );
-};
+//   if (loadedStatus !== 'ready') {
+//     return null;
+//   }
+//   return (
+//     <div>
+//       <div id="dropin-container"></div>
+//       {!!bindStatus &&
+//         <Button onClick={onRequestPaymentMethod}>
+//           Request payment method
+//         </Button>
+//       }
+//     </div>
+//   );
+// };
 
 const PlansIndex = withPageContext((props) => {
   return (
